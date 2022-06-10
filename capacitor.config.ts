056376185a6a536d53969eae40004ba78c82b60e
Tyler Bauman <t.bauman@vehikl.com>
2022-06-10 09:02:06 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spotify-ionic-react',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
